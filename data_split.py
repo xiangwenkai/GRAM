ebd016b1f79dcda5996339f01c581024e3ed065f
xiangwenkai <93317912+xiangwenkai@users.noreply.github.com>
2024-06-03 09:15:59 +0800

    Update data_split.py

--- data_split.py
+++ data_split.py
@@ -5,7 +5,7 @@ import pickle
 import torch
 
 for split in ['train', 'val', 'test']:
-    path = './data/p_sub/{}'.format(split)
+    path = './data/processed/{}'.format(split)
     files = os.listdir(path)
     ids = [x.split('_')[0] for x in files]
     ids = list(set(ids))
@@ -18,11 +18,11 @@ for split in ['train', 'val', 'test']:
         [mol, g, G, pos, dist, angle, edge_index, idx_i, idx_j, idx_k] = pickle.load(open(path+'/{}'.format(name), 'rb'))
 
         if name_dict[name_id] <= 0.8:
-            destination_file = "./data/p_sub/train/{}".format(name)
+            destination_file = "./data/processed/train/{}".format(name)
         elif name_dict[name_id] <= 0.9:
-            destination_file = "./data/p_sub/val/{}".format(name)
+            destination_file = "./data/processed/val/{}".format(name)
         else:
-            destination_file = "./data/p_sub/test/{}".format(name)
+            destination_file = "./data/processed/test/{}".format(name)
         with open(destination_file, "wb") as f:
             pickle.dump((mol, g, G, pos, dist, angle, edge_index, idx_i, idx_j, idx_k), f)
 
